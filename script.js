8438148ac521ccfc24034408ed4ba03fdefb6425
sholly431 <onanibosifiyin@gmail.com>
2024-09-25 13:33:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+/*
+let johnWeight=92;
+ let johnHeight=1.95;
+let markWeight=78;
+let markHeight=1.69;
+console.log(johnWeight/johnHeight**2);
+console.log(markWeight/markHeight**2);
+console.log((markWeight/markHeight**2)>(johnWeight/johnHeight**2));
+/*let markweight=95;
+let markheight=1.88;
+let johnweight=85;
+let johnheight=1.76;
+let BMImark=markweight/markheight**2;
+let BMIjohn=johnweight/johnheight**2;
+console.log(markweight/markheight**2);
+console.log(johnweight/johnheight**2);
+console.log((BMImark)>(BMIjohn));
+
+const firstName= 'olusola';
+const job= 'programmer';
+const birthYear=1999;
+const year=2024;
+const olusola="I'm "  +  firstName + ', a ' + (year - birthYear)
++ ' year old ' + job + '!';
+console.log(olusola);
+
+const age =15;
+if(age>= 18)
+{console.log('sarah can start driving license');}
+else{
+  const yearsLeft= 18 - age;
+console.log('sarah is too young.Wait another + (yearsLeft)years:)');
+}
+let markweight=95;
+let markheight=1.88;
+let johnweight=85;
+let johnheight=1.76;
+let BMImark=markweight/markheight**2;
+let BMIjohn=johnweight/johnheight**2;
+console.log(markweight/markheight**2);
+console.log(johnweight/johnheight**2);
+console.log((BMImark)>(BMIjohn));
+if(BMImark>BMIjohn){console.log("BMImark is higher than BMIjohn");}
+else {console.log("BMIjohn is higher than BMImark");}
+console.log(BMImark + ' is '  +  ' higher ' + ' than ' + BMIjohn + '!');
+let n= '1'+1;
+n=n-1;
+console.log(n);
+const favorite=Number(prompt("what's your favorite number?"));
+console.log(favorite);
+console.log (typeof favorite);
+if(favorite===67) console.log('67 is an amazing number')
+  else if(favorite===87) console.log('87 is also a cool number')
+else if(favorite===47)console.log('47 is also a cool number')
+  else{ console.log('Number is not 67 or 87 or 47')}*/
+const dolphinsScore =(96,108,89);
+const koalasScore=(88,91,110);
+const dolphinsAverage=( (96+108+89)/3);
+console.log(dolphinsAverage);
+const kaolasAverage=( (88+91+110)/3);
+console.log(kaolasAverage);
+if(dolphinsAverage>kaolasAverage){console.log('dolphin win the trophy');}
+else if(dolphinsAverage===kaolasAverage){console.log('Both of them won the game');}
+else if(kaolasAverage>dolphinsAverage){console.log('kaolas win the game');}
+
+const dolphinsBonus =(97,112,101);
+const koalasBonus=(109,95,123);
+const dolphinsBonusAverage=( (97+112+101)/3);
+console.log(dolphinsBonusAverage);
+const kaolasBonusAverage=( (109+95+123)/3);
+console.log(kaolasBonusAverage);
+if(dolphinsBonusAverage<kaolasBonusAverage){console.log('kaolas win the trophy');}
+if(dolphinsBonusAverage>100){console.log('team dolphin is entitled to the game');}
+else if(kaolasBonusAverage>100){console.log('team kaolas is  entitled to the game');}
+else if(dolphinsBonusAverage===kaolasBonusAverage){console.log('both of them are entitled')}
+
+const Monday=('plan course structure')
+const Tuesday=('prepare theory videos')
+const Wednesday=('write code examples')
+const Thursday=('record videos')
+console.log(Monday);
+
+function  calculateAge(yearOfBirth)
+{var age=2024-yearOfBirth;
+   return age;}
+  var ageJohn=calculateAge(1999);
+  var agefiyin=calculateAge(1998);
+  var agemary=calculateAge(1989);
+  console.log(agemary);
+
+  function yearsUntilRetirement(name,year){var age=calculateAge(year)
+var age=calculateAge(year);
+var retirement=65-age;
+console.log(retirement);
+  }                                     
+  
